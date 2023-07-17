57b1acdc71aba26d17c8fbe5e1a09631f8e1edbe
NAGAVENIGOWDA <nagavenigowda017@gmail.com>
2023-07-17 12:48:44 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const connectToMongo =require('./db')
+const express = require('express')
+connectToMongo();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`inotebook listening on port ${port}`)
+})
