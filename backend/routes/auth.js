a0de98e0277256f189d3ff522886f3b5f0a00357
NAGAVENIGOWDA <nagavenigowda017@gmail.com>
2023-07-18 12:22:30 +0530

    initial commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,44 @@
-const express=require('express')
-const router =express.Router();
-const User =require('../models/User')
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.get('/',(req,res)=>{
-    console.log(req.body)
-    const user=User(req.body);
-    user.save()
-    res.send(req.body)
-  
-})
+router.post(
+  "/createuser",
+  [
+    body("name", "enter a valid name").isLength({ min: 3 }),
+    body("email", "enter a valid email Id").isEmail(),
+    body("password", "enter a strong password").isLength({ min: 5 }),
+  ],//validation
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-module.exports=router
+    try {
+      let user = await User.findOne({ email: req.body.email });  //if users with same mail id
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "sorry user with this email alraedy exists" });
+      }
+
+      //User is the schema , User.craete :This function is used to create a new user record in the database based on the provided data
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.json(user);
+    } 
+    
+    catch (error) {
+      console.error(error.message);
+      res.status(500).send("some error occured");
+    }
+    
+  }
+);
+
+module.exports = router;
